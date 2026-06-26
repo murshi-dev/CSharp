83a2795cf6f9c672bcf665699d54348359a6b76d
murshi <90964789+murshi-dev@users.noreply.github.com>
2026-06-26 11:25:21 +0800

    c
    c

---
+++ Apr26/LINQConsole/ContactsManagerApp/ContactsManagerApp/ContactManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Xml.Linq;
+
+class ContactManager
+{
+    private List<Contact> contactList;
+    public ContactManager()
+    {
+        contactList = new List<Contact>();
+    }
+    //method to add names to the list
+    public void Add(string name)
+    {
+        contactList.Add(new Contact{ Name = name });
+    }
+    //method to display the names from the list
+    public List<Contact> GetContacts()
+    {
+        return contactList.ToList();
+    }
+    //method to check if the name exists
+    public bool ListItemExists(string name)
+    {
+        return contactList.Any(c=>c.Name.Equals(name));
+    }
+    //method to edit the name in the list
+    public void EditContact(string oldName, string newName)
+    {
+        var contactToEdit = 
+                contactList.FirstOrDefault(c => c.Name.Equals(oldName));
+        contactToEdit.Name = newName;
+    }
+    //method to remove name from the list
+    public bool RemoveContact(string name)
+    {
+        var contactToRemove =
+        contactList.FirstOrDefault(c => c.Name.Equals(name));
+        contactList.Remove(contactToRemove);
+        return true;
+    }
+}
---
+++ Apr26/LINQConsole/ContactsManagerApp/ContactsManagerApp/Program.cs
@@ -0,0 +1,74 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+        ContactManager contactManager = new ContactManager();
+        bool option = true;
+        while (option)
+        {
+            Console.WriteLine("1. Add Contact");
+            Console.WriteLine("2. Display Contact");
+            Console.WriteLine("3. Edit Contact");
+            Console.WriteLine("4. Remove Contact");
+            Console.WriteLine("Enter an option: ");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    Console.WriteLine("Enter a name to add:");
+                    string nameToAdd = Console.ReadLine();
+                    contactManager.Add(nameToAdd);
+                    Console.WriteLine($"{nameToAdd} added to list");
+                    break;
+                case "2":
+                    Console.WriteLine("Contacts in the list are:");
+                    var contact = contactManager.GetContacts();
+                    if (contact.Any())
+                    {
+                        contact.ForEach(c => Console.WriteLine(c.Name));
+                    }
+                    else
+                    {
+                        Console.WriteLine("List is empty");
+                    }
+                    break;
+
+                case "3":
+                    Console.WriteLine("Enter a name to edit:");
+                    string oldName = Console.ReadLine();
+
+                    if (contactManager.ListItemExists(oldName))
+                    {
+                        Console.WriteLine("Enter the new name name to edit:");
+                        string newName = Console.ReadLine();
+                        contactManager.EditContact(oldName, newName);
+                        Console.WriteLine($"{oldName} edited in the list");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{oldName} does not exist in the list");
+                    }
+                    break;
+
+                case "4":
+                    Console.WriteLine("Enter the name to remove from the list:");
+                    string nameToRemove = Console.ReadLine();
+                    if (contactManager.ListItemExists(nameToRemove))
+                    {
+                        contactManager.RemoveContact(nameToRemove);
+                        Console.WriteLine($"{nameToRemove} removed from the list");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{nameToRemove} does not exist in the list");
+                    }
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid input");
+                    break;
+            }
+        }
+    }
+}
